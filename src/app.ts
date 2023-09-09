647471bc786c564aae8907add052a8d819b5889f
gabeFrancisco <gabegit2@gmail.com>
2023-09-08 22:51:27 -0300

    added cors | created controllers and services folder

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import cors from 'cors'
 import bodyParser from 'body-parser';
+import dotnev from 'dotenv'
 import { router } from './router'
 
+dotnev.config()
+
 const app = express()
+
+app.use(cors({
+  credentials: false,
+  origin: process.env.ORIGIN
+}))
 
 app.use(bodyParser.json());
 app.use(router)
