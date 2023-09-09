dd7c51bfb958d9e789c3a38772f37eb888f3ffe6
gabeFrancisco <gabegit2@gmail.com>
2023-09-09 20:54:19 -0300

    interfaces folder, BaseEntity and Pedal entity, Categories enum

---
+++ src/interfaces/Pedal.ts
@@ -0,0 +1,10 @@
+import { Categories } from "../enums/Categories";
+import { BaseEntity } from "./BaseEntity";
+
+export interface Pedal extends BaseEntity{
+  manufactor: string
+  type: string
+  model: string
+  year: string
+  category: Array<Categories>
+}
