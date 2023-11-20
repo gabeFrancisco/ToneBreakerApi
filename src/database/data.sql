c32de0716955cfdd61b80eb07ed004e30120774e
gabeFrancisco <gabegit2@gmail.com>
2023-11-19 22:08:35 -0300

    db entities test and inner join

--- src/database/data.sql
+++ src/database/data.sql
@@ -10,6 +10,19 @@ CREATE TABLE pedals(
     updatedAt TIMESTAMP
 );
 
+insert into pedals(manufactor, model, year, description, imageUrl, size, createdAt) values (
+    'Bk Buttler',
+    'Tube Driver',
+    '1978',
+    'A classic overdrive/boost pedal with tubes',
+    null,
+    'Large',
+    NOW();
+);
+
+insert into pedal_categories(pedal_id, category_id) values (1, 1);
+insert into pedal_categories(pedal_id, category_id) values (2, 1);
+
 CREATE TABLE categories(
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
@@ -39,10 +52,10 @@ CREATE TABLE controls(
 
 CREATE TABLE pedal_categories(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    description TEXT NOT NULL,
-    createdAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP
+    pedal_id INT NOT NULL,
+    category_id INT NOT NULL,
+    FOREIGN KEY (pedal_id) REFERENCES pedals(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
 CREATE TABLE pedal_tones(
@@ -55,3 +68,20 @@ CREATE TABLE pedal_tones(
 
 -- pedalId INT NOT NULL,
 -- FOREIGN KEY (pedalId) REFERENCES pedals(id),
+
+insert into categories(name, description, createdAt) values (
+    'Overdrive', 'A pedal that clips the guitar signal and saturates, emulating tube amps', 
+    NOW()
+);
+
+insert into tones(name, description, createdAt) values (
+    'Warm',
+    'A hot, brown and vintage tone, with more bass and mid frequencies.',
+    NOW()
+);
+
+insert into tones(name, description, createdAt) values (
+    'Tube',
+    'Warm, dynamic and colourful vintage tone that comes from tubes.',
+    NOW()
+);
